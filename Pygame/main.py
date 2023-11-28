13937e58b95788e63ef73490e0f4e0f36013d0fb
Gustavo Hess <gustavohess1@gmail.com>
2023-11-28 17:01:09 -0300

    queue reading ready

--- Pygame/main.py
+++ Pygame/main.py
@@ -3,31 +3,31 @@ import serial
 import threading
 from queue import Queue
 from tank import Tank
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 # Initialize Pygame
 pygame.init()
 
 # Set up display
-width, height = 800, 600
+width, height = 1400, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tank Simulator")
 
 # Create a single tank (bigger in scale, moved to the left)
-tank_width = 250  # Original width
-tank_height = 400  # Original height
-tank_x = 50  # Move it to the left
-tank_y = (height - tank_height) // 2  # Center vertically
-
-tank = Tank(tank_x, tank_y, tank_width, tank_height, color=(0, 0, 128))  # Dark Blue
+tank_width = 250
+tank_height = 400
+tank_x = width/2 - tank_width/2 + 100
+tank_y = 50  # Adjusted position to be at the top
+tank = Tank(tank_x, tank_y, tank_width, tank_height, color=(0, 0, 128))
 
 # Slider parameters
-slider_width = 20  # Make it vertical
-slider_height = 500  # Make it vertical
-slider_x = width - 100  # Place it on the right side
-slider_y = (height - slider_height) // 2  # Center the slider vertically
+slider_width = 20
+slider_height = 540
+slider_x = width - 120  # Adjusted to leave space on the right
+slider_y = 20  # Adjusted position to be at the top
+slider_levels = ["Full", "High", "Medium-High", "Medium", "Medium-Low", "Low", "Very Low", "Empty"]
 
-# Slider levels and corresponding values (reversed order)
-slider_levels = ["Full", "High", "Medium", "Low", "Empty"]
 slider_value = "Empty"
 
 # Font setup for slider labels
@@ -38,10 +38,13 @@ ser = serial.Serial('COM1', 115200, 7, 'O', 1, timeout=1)
 
 # Dictionary to map slider value to binary message
 slider_to_binary = {
-    "Full": '6',
-    "High": '5',
-    "Medium": '3',
+    "Full": '7',
+    "High": '6',
+    "Medium-High": '5',
+    "Medium": '4',
+    "Medium-Low": '3',
     "Low": '2',
+    "Very Low": '1',
     "Empty": '0',
 }
 
@@ -51,17 +54,26 @@ def draw_slider(screen):
     slider_pos = slider_levels.index(slider_value)
     slider_pos = int((slider_pos / (len(slider_levels) - 1)) * slider_height)
 
-    # Draw a dark blue ball indicator
-    ball_radius = 10  # Adjust the radius as needed
+    ball_radius = 10
     ball_x = slider_x + slider_width / 2
     ball_y = slider_y + slider_pos
     pygame.draw.circle(screen, (0, 0, 128), (int(ball_x), int(ball_y)), ball_radius)
+
 
 # Update vertical slider value based on mouse position
 def update_slider(mouse_y):
     normalized_y = max(0, min(1, (mouse_y - slider_y) / slider_height))
     closest_level = min(range(len(slider_levels)), key=lambda y: abs(y - normalized_y * (len(slider_levels) - 1)))
     return slider_levels[closest_level]
+
+def update_graph(ax, data_array):
+    ax.clear()
+    ax.plot(range(len(data_array)), data_array, marker='o', linestyle='-')
+    ax.set_xlim(max(0, len(data_array) - 20), len(data_array))
+    ax.set_ylim(0, max(data_array) + 10)
+    ax.set_title("Tank Water Level Over Time")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Water Level")
 
 # Function to receive serial data in a separate thread
 def receive_serial(data_queue):
@@ -83,16 +95,30 @@ data_thread = threading.Thread(target=receive_serial, args=(data_queue,))
 data_thread.daemon = True
 data_thread.start()
 
-
-# Function to simulate receiving data in a separate thread, it is a mock up
+# Function to simulate receiving data in a separate thread, it is a mock-up
 def simulate_receive_serial(mock_data_queue):
-    for data in ["100", "150", "200", "180", "250"]:
-        pygame.time.delay(300)
-        mock_data_queue.put(data)
+    # for data in ["100", "150", "200", "180", "250", "275", "295", "305", "320", "340", "360", "380", "392", "400"]:
+    #     pygame.time.delay(50)
+    #     mock_data_queue.put(data)
+
+        # Function to simulate receiving data in a separate thread, it is a mock-up
+    char_array = ["0", "1", "3", ",", "0", "c", "m", "#",
+                  "0", "1", "3", ",", "5", "c", "m", "#",
+                  "0", "1", "4", ",", "0", "c", "m", "#",
+                  "0", "1", "4", ",", "5", "c", "m", "#",
+                  "0", "1", "5", ",", "0", "c", "m", "#"
+                  ]
+    for char in char_array:
+        pygame.time.delay(50)
+        mock_data_queue.put(char)
     mock_data_queue.put("")  # Add an empty string to simulate receiving an empty data
 
 # Create a mock queue
 mock_data_queue = Queue()
+
+# Set up matplotlib for real-time graph
+fig, ax = plt.subplots(figsize=(5, 3))  # Adjust the figsize parameter as needed
+canvas = FigureCanvas(fig)
 
 # Main game loop
 running = True
@@ -109,48 +135,62 @@ while running:
                 slider_x - 5 < event.pos[0] < slider_x + slider_width + 5
                 and slider_y - 5 < event.pos[1] < slider_y + slider_height + 5
             ):
-                # Update the slider value based on the mouse position
                 slider_value = update_slider(event.pos[1])
-
-                # Get the binary message from the dictionary
                 binary_data = slider_to_binary.get(slider_value, '0000000')
                 print(slider_value, binary_data, binary_data.encode())
-                # Send the binary data to the hardware
                 ser.write(binary_data.encode())
 
-  
-    #simulate_receive_serial(mock_data_queue)  #uncomment this line to use mock data
+
+    # simulate_receive_serial(mock_data_queue)  # Uncomment this line to use mock data
+
+    # if not mock_data_queue.empty():
+    #     data = mock_data_queue.get()  # Uncomment this line to use mock data
+    #     if data:  # Only append non-empty data to the array
+    #         received_data_array.append(int(data))
+    # print("Received Data Array:", received_data_array)
 
 
-    if not data_queue.empty(): 
-    # if not mock_data_queue.empty():  #uncomment this line to use mock data
-        data = data_queue.get()
-        #data = mock_data_queue.get() #uncomment this line to use mock data
-        if data:  # Only append non-empty data to the array
-            received_data_array.append(int(data))
+    #IMPORTANT, IF NOT MOCKING UP DATA, COMMENT FROM 133 UNTILL 139, OTHERWISE UNCOMMENT FROM 145 UNTILL 153    
 
-    # Draw vertical slider
-    draw_slider(screen)
+
+    if data_queue.qsize() >= 8:
+            data = ""
+            for _ in range(8):
+                data += data_queue.get()
+
+            # Replace comma with period and extract the relevant part, e.g., "013.0"
+            important_data = data.replace(',', '.')[1:5]
+            received_data_array.append((20-float(important_data))*20)
+            print("Received Data Array:", received_data_array)
 
     # Draw tank (bigger in scale, moved to the left) based on the last value in received_data_array
     if received_data_array:
         last_value = received_data_array[-1]
         tank.update_water_level(last_value)  # Set the tank's water level based on the last value in the array
 
+    # Draw tank
     tank.draw(screen)
 
     # Draw slider labels (moved even more to the left)
     for i, level in enumerate(slider_levels):
         label = font.render(level, True, (0, 0, 0))
-        # Move labels even more to the left
-        label_rect = label.get_rect(midleft=(slider_x - 60 - 10 * (width / 800), slider_y + i * (slider_height / (len(slider_levels) - 1))))
+        label_rect = label.get_rect(
+            midleft=(slider_x - 100 - 10 * (width / 800), slider_y + i * (slider_height / (len(slider_levels) - 1))))
         screen.blit(label, label_rect)
 
-    # Print the received data array (you can modify this part as needed)
-    print("Received Data Array:", received_data_array)
+    # Update and render the real-time graph
+    update_graph(ax, received_data_array)
+    canvas.draw()
+    renderer = canvas.get_renderer()
+    raw_data = renderer.tostring_rgb()
+    size = canvas.get_width_height()
+    graph_surface = pygame.image.fromstring(raw_data, size, "RGB")
+    screen.blit(graph_surface, (50, 100))  # Adjust the position as needed
 
-    pygame.time.delay(100)
+    # Draw slider
+    draw_slider(screen)
 
+    # pygame.time.delay(100)
     pygame.display.flip()
 
 # Close the serial port
